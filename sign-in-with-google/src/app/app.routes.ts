4b55d655171d3faf3530fc1eb0d5b3944b50dfb7
puja lakhe <lakhepuza924@gmail.com>
2024-09-30 13:28:50 +0545

    feat:login route

--- sign-in-with-google/src/app/app.routes.ts
+++ sign-in-with-google/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'error', component: ErrorComponent },
