c7767408d6705cd94048c0185a1519307ceea39b
puja lakhe <lakhepuza924@gmail.com>
2024-09-30 12:04:40 +0545

    dashboard-component

--- sign-in-with-google/src/app/components/dashboard/dashboard.component.ts
+++ sign-in-with-google/src/app/components/dashboard/dashboard.component.ts
@@ -2,12 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserCardComponent } from '../user-card/user-card.component';
 import { CommonModule } from '@angular/common';
+import { MatLabel } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 declare var handleSignout: any; // Declare the global function to avoid TypeScript errors
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [UserCardComponent, CommonModule],
+  imports: [
+    UserCardComponent,
+    CommonModule,
+    MatLabel,
+    MatButtonModule,
+    MatButtonModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
