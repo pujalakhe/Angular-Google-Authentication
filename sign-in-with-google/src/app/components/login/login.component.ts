dfe4908abe583a1691272bb6229fd920cefe5840
puja lakhe <lakhepuza924@gmail.com>
2024-09-30 12:05:58 +0545

    use of angular material component

--- sign-in-with-google/src/app/components/login/login.component.ts
+++ sign-in-with-google/src/app/components/login/login.component.ts
@@ -1,21 +1,48 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  public username: string = '';
-  public password: string = '';
-  constructor() {}
+  loginForm: FormGroup = new FormGroup({});
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
   onSubmit() {
-    console.log(`Username: ${this.username}, Password: ${this.password}`);
+    this.loginForm.markAllAsTouched();
+    if (!this.loginForm.valid) {
+      return;
+    }
   }
 }
